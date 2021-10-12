f3221a98e02369fb1442340f7086a7a3238ea146
Jitender Sharma <jitender.sharma@energyexemplar.com>
2021-10-12 23:17:16 +0530

    redefined the top and left bar

--- src/Components/Core/TopNavBar.vue
+++ src/Components/Core/TopNavBar.vue
@@ -1,8 +1,16 @@
 <template>
-  <v-app-bar color="deep-purple accent-4" light elevation="0">
+  <v-app-bar class="deep-purple accent-2" light elevation="0">
     <v-toolbar-title class="white--text">Gaadi Ghoda</v-toolbar-title>
     <v-spacer />
+    <v-avatar class="mr-2" size="30px">
+      <img
+        class="avatar-img"
+        src="https://www.gravatar.com/avatar/?d=mp"
+        alt="Avatar"
+      />
+    </v-avatar>
     User_Name
+    <v-icon color="white" size="26" class="ml-2">mdi-power</v-icon>
   </v-app-bar>
 </template>
 
@@ -12,4 +20,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.avatar-img {
+  border: 2px solid white;
+}
+</style>
