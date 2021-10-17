c37c0067fa8bdb87bc3f80474090a4f085abe129
Jitender Sharma <jitender.sharma@energyexemplar.com>
2021-10-17 17:00:09 +0530

    initial dummy vuex setup

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,15 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+const state = {
+  posts: [],
+};
+
+const getters = {};
+
+const mutations = {
+  SET_POSTS(state, posts) {
+    state.posts = posts;
+  },
+};
+
+const actions = {
+  getPosts({ commit }) {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      commit("SET_POSTS", response.data);
+    });
+  },
+};
+
 export default new Vuex.Store({
-  state: {
-    count: 0,
-  },
-  mutations: {
-    increment(state) {
-      state.count++;
-    },
-  },
+  state,
+  getters,
+  mutations,
+  actions,
 });
